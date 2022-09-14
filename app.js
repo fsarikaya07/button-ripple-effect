d8de606b85678ba6f25a6d03042560b81c0f540d
Fatih Sarıkaya <f.sarikaya@gmail.com>
2022-09-15 02:32:39 +0300

    end

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const btnEl = document.getElementById("btn");
 
-btnEl.addEventListener("mouseover", () => {
-  btnEl.style.backgroundColor = "black";
-  btnEl.style.transition = "all 0.5s";
+btnEl.addEventListener("mouseover", (axis) => {
+  const x = axis.pageX- btnEl.offsetLeft;
+  const y = axis.pageY- btnEl.offsetTop;
+
+  btnEl.style.setProperty("--xPos", x + "px");
+  btnEl.style.setProperty("--yPos", y + "px");
 });
 
-btnEl.addEventListener("mouseout", () => {
-  btnEl.style.backgroundColor = "pink";
-  btnEl.style.transition = "all 0.5s";
-});
+// btnEl.addEventListener("mouseover", () => {
+//   btnEl.style.backgroundColor = "black";
+//   btnEl.style.transition = "all 0.5s";
+// });
+
+// btnEl.addEventListener("mouseout", () => {
+//   btnEl.style.backgroundColor = "pink";
+//   btnEl.style.transition = "all 0.5s";
+// });
